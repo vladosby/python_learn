9bf492148f774bb0d2cd7cc703655433bfbe8b45
Uladzislau Kiyeuski <vladislavki@playtika.com>
2022-06-03 23:04:57 +0200

    Slicing examples

---
+++ src/objects_and_data_structure.py
@@ -0,0 +1,18 @@
+string = "hello world"
+
+# slicing [start:stop:step]
+
+print(string[-2])
+# l
+print(string[0:-2])
+# hello wor
+print(string[0:len(string):2])
+# hlowrd
+print(string[4:])
+# o world
+print(string[:4])
+# hell
+print(string[::2])
+# hlowrd
+print(string[::-1])
+# dlrow olleh
