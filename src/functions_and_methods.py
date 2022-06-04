9c04d4d52ca5602cc3e83ee0db576cbebe257a10
Uladzislau Kiyeuski <vladislavki@playtika.com>
2022-06-04 19:31:57 +0200

    Lambda, map, filter

--- src/functions_and_methods.py
+++ src/functions_and_methods.py
@@ -28,6 +28,35 @@ def my_func4(*args, **kwargs):
     print(args)
     print(kwargs)
 
-my_func4(1,2,3,4, key1="aaa", key2="bbb")
+
+my_func4(1, 2, 3, 4, key1="aaa", key2="bbb")
+
+
 # (1, 2, 3, 4)
 # {'key1': 'aaa', 'key2': 'bbb'}
+
+def square(num):
+    return num ** 2
+
+
+test_array1 = [1, 2, 3, 4, 5]
+result1 = map(square, test_array1)
+print(result1)
+# <map object at 0x7f84a802c9a0>
+print(list(result1))
+
+
+# [1, 4, 9, 16, 25]
+
+def check_even(num):
+    return num % 2 == 0
+
+
+filter1 = filter(check_even, test_array1)
+print(filter1)
+# <filter object at 0x7fb5380c3610>
+print(list(filter1))
+# [2, 4]
+
+print(list(map(lambda num: num ** 2, test_array1)))
+# [1, 4, 9, 16, 25]
