b4e98e19d698ba0889f601fa12405c5a1134c49c
Uladzislau Kiyeuski <vladislavki@playtika.com>
2022-06-05 00:15:17 +0200

    Error handling

---
+++ src/errors_and_exceptions.py
@@ -0,0 +1,29 @@
+def square(num):
+    if num < 10:
+        raise Exception("Number too big")
+    else:
+        return num ** 2
+
+
+try:
+    print(f"function result {square(11)}")
+except Exception:
+    print("Exception caught")
+else:
+    print("No exception caught")
+finally:
+    print("I am working always")
+# function result 121
+# No exception caught
+# I am working always
+
+try:
+    print(f"function result {square(5)}")
+except Exception:
+    print("Exception caught")
+else:
+    print("No exception caught")
+finally:
+    print("I am working always")
+# Exception caught
+# I am working always
